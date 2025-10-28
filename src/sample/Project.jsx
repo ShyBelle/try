00a7a5038e63dc298c992bdefe579f631c18f85a
ShyBelle <shybelle.castor16@gmaiil.com>
2025-10-28 21:26:56 +0800

    updates

--- src/sample/Project.jsx
+++ src/sample/Project.jsx
@@ -1,37 +1,51 @@
-import {Box, Stack} from "@mui/material";
-
+import {Box, Grid , Typography} from "@mui/material";
 
 function Project(){
     return(
-        <Stack direction="row" spacing={0}>
-            <Box
-                sx={{ flexGrow: 1,
-                    border: '1px solid black',
-                    height: '100px',
-                    width: '100px',
-                    mt: '16px'
-                }}
-            >
-            </Box>
-            <Box
-                sx={{ flexGrow: 1,
-                    border: '1px solid black',
-                    height: '100px',
-                    width: '100px',
-                    mt: '16px'
-                }}
-            >
-            </Box>
-            <Box
-                sx={{ flexGrow: 1,
-                    border: '1px solid black',
-                    height: '100px',
-                    width: '100px',
-                    mt: '16px'
-                }}
-            >
-            </Box>
-        </Stack>
+        <Grid container spacing={1}>
+            <Grid item xs={4}>
+                <Box
+                    sx={{ flexGrow: 1,
+                        border: '1px solid black',
+                        height: '100px',
+                        mt: '16px'
+                    }}
+                    fullWidth
+                >
+                </Box>
+            </Grid>
+            <Grid item xs={4}>
+                <Box
+                    sx={{ flexGrow: 1,
+                        border: '1px solid black',
+                        mt: '16px'
+                    }}
+                >
+                    <Box component="section" sx={{ p: 3, border: '4px dashed #000' , backgroundColor: 'gray' }}>
+                        <Typography variant="h6" gutterBottom sx={{color:'white'}}>
+                            Shybelle
+                        </Typography>
+                        <Typography variant="span" gutterBottom>
+                            subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                            blanditiis tenetur
+                        </Typography>
+                    </Box>
+
+                </Box>
+            </Grid>
+            <Grid item xs={4}>
+                <Box
+                    sx={{ flexGrow: 1,
+                        border: '1px solid black',
+                        mt: '16px'
+                    }}
+                >
+                    <Box component="section" sx={{ p: 3, border: '4px dashed #000' }}>
+                        shy
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>
     )
 }
 export default Project
